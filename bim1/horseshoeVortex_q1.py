bfbfadd2a21ebf36da2c73a745ab6fe28b045cc5
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2026-04-12 15:12:45 -0300

    Ajusta o tamanho da figura para o gráfico da componente z da velocidade induzida por um vórtice em forma de ferradura

--- bim1/horseshoeVortex_q1.py
+++ bim1/horseshoeVortex_q1.py
@@ -32,7 +32,7 @@ Gamma = 1.0  # Circulation
 y = np.linspace(-2, 2)
 w = Gamma*L/ (4*np.pi) /(y**2-L**2/4)
 
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(6, 4))
 plt.plot(y, w, 'darkblue', linewidth=2)
 plt.xlabel('y [m]')
 plt.ylabel('w [m/s]')
