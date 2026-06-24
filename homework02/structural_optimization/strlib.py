74a3470a9b3ce9b62697922eb753cc9206cbe49a
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2026-06-24 14:11:21 -0300

    Adiciona classe StructuralOpt com atributos e métodos para otimização estrutural

--- homework02/structural_optimization/strlib.py
+++ homework02/structural_optimization/strlib.py
@@ -1,9 +1,29 @@
 import numpy as np
 from scipy.optimize import root
+from dataclasses import dataclass, field
 
+@dataclass
+class StructuralOpt:
+    R:float             # [m]
+    E:float             # [Pa]             
+    sigma_y: float      # [Pa]
+    rho: float          # [kg/m^3]
+    F1: float           # [N]
+    F2: float           # [N]
+    rhoKs: float = 10   
+    F: np.ndarray = field(init=False, default_factory=lambda: np.array([]))
+    A: np.ndarray = field(init=False, default_factory=lambda: np.array([]))
+    
+    def __post_init__(self):
+        # build force vectors
+        self.F = np.array([self.F1, 0, self.F2, 0])
+         
 
-def resfun(A,p,F):
-    return A@p - F 
+    def _build_stiffness_matrix(self):
+        pass
 
-def objfun(ta,tb):
-    pass
+    def _resfun(self, p):
+        return self.A@p - self.F 
+
+    def objfun(self, ta,tb):
+        pass
