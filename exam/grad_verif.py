8430b85ecc7f8284fa4563d44d26905693c49a58
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2026-07-05 22:24:35 -0300

    Adicionar arquivo grad_verif.py com importações necessárias

---
+++ exam/grad_verif.py
@@ -0,0 +1,7 @@
+#%%
+import numpy as np
+
+from asa_module import asa_module as asa # type:ignore
+from asa_module_d import asa_module_d as asa_d # type:ignore
+from asa_module_b import asa_module_b as asa_b # type:ignore
+#%%
