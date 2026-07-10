fix: update state variable scaling factors, improve root solver tolerance, and add validation checks for adjoint results

exam/5.4_5.5_solve_physics_adjoint_problem.py
@@ -12,6 +12,14 @@ d_val = np.array([ 2.69493469e-02,-8.96491634e-03,9.57299675e-03,-7.85795127e-03
             -9.88815861e-45,-4.23707119e-67,9.57299675e-03,7.85795127e-03,
             2.69493469e-02,8.96491634e-03])
 
+psi_A_val = np.array([-92.62945248, -16.9979671, -16.8732846, -115.59593901])
+psi_S_val = np.array([1.76989719e-04, -4.41232719e-05, 8.87470713e-05, -4.41174285e-05,
+                      -1.53738245e+02, -8.13436714e-08, 8.87470714e-05, 4.41174285e-05,
+                      1.76989720e-04, 4.41232719e-05])
+
+dKSmargin_dtwist_val = np.array([-2.63697857, -1.16166158, -1.16166158, -2.63697857])
+dKSmargin_dt_val = np.array([1.11410770e-03, 6.61735632e+01, 6.61735633e+01, 1.11410770e-03])
+
 #%% 5.4 Solve physics
 twist = np.array([0.0,0.0,0.0,0.0])
 t = np.array([0.005,0.005,0.005,0.005])
@@ -22,14 +30,14 @@ resfunc = lambda stateVars: asaObj._resfunc(desVars=desVars,stateVars = stateVar
 gama0 = np.array([80.0,80.0,80.0,80.0])
 d0 =  np.array([0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001])
 
-stateVars0 = np.hstack([gama0,d0])
+stateVars0 = np.hstack([gama0, d0])
 print(stateVars0)
-sol = root(resfunc,  stateVars0)
+sol = root(resfunc,  stateVars0, options={'xtol': 1e-6})
 print('SOLVE  PHYSICS:')
 print(sol)
 
-gama = sol.x[:asaObj.npanels]/100
-d = sol.x[asaObj.npanels:]/0.1
+gama = sol.x[:asaObj.npanels]/0.1
+d = sol.x[asaObj.npanels:]/100.0
 stateVars = np.hstack([gama,  d])
 # gama
 match = np.allclose(gama, Gama_val)
@@ -57,12 +65,27 @@ psi_ksmargin = sol.x
 psi_A = psi_ksmargin[:asaObj.npanels]
 psi_S = psi_ksmargin[asaObj.npanels:]
 print('Adjoint vars:')
-print(f'psi_A = {psi_A}')
-print(f'psi_S = {psi_S}')
+match_psi_A = np.allclose(psi_A, psi_A_val)
+print(f'psi_A: Code =  {psi_A}')
+print(f'psi_A: 5.5 Values = {psi_A_val}')
+print(f'Match psi_A: {match_psi_A}\n')
+
+match_psi_S = np.allclose(psi_S, psi_S_val)
+print(f'psi_S: Code =  {psi_S}')
+print(f'psi_S: 5.5 Values = {psi_S_val}')
+print(f'Match psi_S: {match_psi_S}\n')
 
 _,dKSmargin_dx = asaObj._adjfunc(desVars,stateVars,psi_ksmargin, 'ksmargin')
 dKSmargin_dtwist = dKSmargin_dx[:asaObj.npanels]
 dKSmargin_dt = dKSmargin_dx[asaObj.npanels:]
-print('Total derivatives')
-print(f'dKSmargin_dtwist = {dKSmargin_dtwist}')
-print(f'dKSmargin_dt = {dKSmargin_dt}')
+
+print('Total derivatives:')
+match_dtwist = np.allclose(dKSmargin_dtwist, dKSmargin_dtwist_val)
+print(f'dKSmargin_dtwist: Code =  {dKSmargin_dtwist}')
+print(f'dKSmargin_dtwist: 5.5 Values = {dKSmargin_dtwist_val}')
+print(f'Match dKSmargin_dtwist: {match_dtwist}\n')
+
+match_dt = np.allclose(dKSmargin_dt, dKSmargin_dt_val)
+print(f'dKSmargin_dt: Code =  {dKSmargin_dt}')
+print(f'dKSmargin_dt: 5.5 Values = {dKSmargin_dt_val}')
+print(f'Match dKSmargin_dt: {match_dt}\n')

exam/asalib.py
@@ -227,8 +227,8 @@ class ASAOptimization:
         t = desVars[self.npanels:]
         
         #Get state vars
-        gama = stateVars[:self.npanels]/100
-        d = stateVars[self.npanels:]/0.1
+        gama = stateVars[:self.npanels]/0.1
+        d = stateVars[self.npanels:]/100.0
         
         out = self._run_asa(twist=twist,gama=gama,t =t, d =d)
         res = np.hstack([out['resllt'], out['resfem']])
@@ -277,14 +277,16 @@ class ASAOptimization:
         
         # Solve physics
         resfunc = lambda stateVars: self._resfunc(desVars, stateVars)
-        gama0 = np.array([80.0,80.0,80.0,80.0])
+        # Use professor's default initial guesses (gama = 80.0, d = 0.001)
+        gama0 = np.array([80.0, 80.0, 80.0, 80.0])
         d0 =  np.array([0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001])
-        stateVars0 = np.hstack([gama0,d0])
-        sol = root(resfunc,stateVars0)
+        # Scale the initial guess for the solver's normalized variables
+        stateVars0 = np.hstack([gama0 * 0.1, d0 * 100.0])
+        sol = root(resfunc, stateVars0, options={'xtol': 1e-6})
         
-        # Split state vars
-        gama = sol.x[:self.npanels]/100
-        d =sol.x[self.npanels:]/0.1
+        # Split state vars (applying same scaling: gama = stateVars/0.1, d = stateVars/100)
+        gama = sol.x[:self.npanels]/0.1
+        d = sol.x[self.npanels:]/100.0
         
         # Call original code
         out = self._run_asa(twist=twist,gama= gama, t = t, d =d)